fbf7b2fcb00a891b02fd3244179ed533447e5171
Vitor Horta <vitor.horta@gmail.com>
2017-03-07 18:56:14 -0300

    Teste pypi

---
+++ es/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'expertsystempython',
+  packages = ['expertsystempython'], # this must be the same as the name above
+  version = '0.1',
+  description = 'expertsystempython',
+  author = 'Vitor Horta',
+  author_email = 'vitor.horta@gmail.com',
+  url = 'https://github.com/vitorhorta/expertSystemPython', # use the URL to the github repo
+  download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['expertsystempython'], # arbitrary keywords
+  classifiers = [],
+)
