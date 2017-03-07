6040474e2d666480b7e5bb060e4911711a39da77
Vitor Horta <vitor.horta@gmail.com>
2017-03-07 19:05:45 -0300

    Fix para pypi

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'es',
   packages = ['es'], # this must be the same as the name above
-  version = '0.4',
+  version = '0.5',
   description = 'expertsystempython',
   author = 'Vitor Horta',
   author_email = 'vitor.horta@gmail.com',
   url = 'https://github.com/vitorhorta/expertSystemPython', # use the URL to the github repo
-  download_url = 'https://github.com/{vitorhorta}/{es}/archive/{0.4}.tar.gz', # I'll explain this in a second
+  download_url = 'https://github.com/vitorhorta/es/archive/0.5.tar.gz', # I'll explain this in a second
   keywords = ['expertsystempython'], # arbitrary keywords
   classifiers = [],
 )
